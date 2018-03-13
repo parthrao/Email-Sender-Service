dab544f76082b275d7f022108da3a13fb5508caa
Parth <parthrao8@gmail.com>
2018-03-13 13:06:58 -0400

    name changes

--- src/com/mfgit/mailservice/tests/testHarness.java
+++ src/com/mfgit/mailservice/tests/testHarness.java
@@ -32,10 +32,10 @@ public class testHarness {
 		sm.setEmailBody("<html>Hello,<br><br>[WADR] Template was not found !<br><br>"
 		+ "Program did not find the [WADR] Excel Template on Vendor's FTP. Please place the Template<br><br>"
 		+"<table border='1'><tr bgcolor='#008080'align='center'><FONT COLOR='WHITE'><th>WORK WEEK</th><th>DATE-TIME(IST)</th><th>VENDOR</th><th>RECORDS IMPORTED</th></font></tr><tr align='center'><td>FY16Q4WW50</td><td>12-06-2016 19:11:38</td><td>ASE-CL</td><FONT COLOR='WHITE'><td bgcolor='red'>0</td></FONT></tr></table><br><br>"
-		+ "In case of any issues / queries please email to - <a href='mit.appsupportdesk@maximintegrated.com'>MIT.AppSupportDesk@maximintegrated.com</a><br><br>"
-		+ "- | Maxim Integrated | GNR | </html>");
-		sm.setSubject("EMPTY or NO File Found | ASE-CL | FY16Q4WW50");
-		sm.addAddress("Nitin.Dodia@maximintegrated.com");
+		+ "In case of any issues / queries please email to - <a href='some@example.com'>some@example.com</a><br><br>"
+		+ "- </html>");
+		sm.setSubject("EMPTY or NO File Found");
+		sm.addAddress("some@example.com");
 		fc.sendRequest(sm);
 	}
 	
@@ -45,7 +45,7 @@ public class testHarness {
 		
 		mc.init("tcp://localhost:61616", "MailService", 20000);
 		
-		mc.simpleSend("kkhushali.dave@gmail.com", "singleton Test", "This is the reminder to provide the file");
+		mc.simpleSend("parthrao8@gmail.com", "singleton Test", "This is the reminder to provide the file");
 	}
 	
 	
